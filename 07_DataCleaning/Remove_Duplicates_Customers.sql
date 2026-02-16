24ac286e7ce48d7b44e0d2fccca3b0216dbd72ca
Recep Pekdas <receppekdas3460@gmail.com>
2026-02-16 16:05:07 +0300

    feat: add duplicate removal with CTE, transaction and error handling

---
+++ 07_DataCleaning/Remove_Duplicates_Customers.sql
@@ -0,0 +1,52 @@
+﻿USE mssql_data_cleaning_lab_test;
+GO
+
+SET NOCOUNT ON;
+
+-- Step 1: Duplicate kayıtları tespit et
+WITH DuplicateCTE AS
+(
+    SELECT
+        CustomerID,
+        FullName,
+        Email,
+        City,
+        ROW_NUMBER() OVER (
+            PARTITION BY FullName, Email, City
+            ORDER BY CreatedAt ASC
+        ) AS RowNum
+    FROM dbo.Customers
+)
+SELECT *
+FROM DuplicateCTE
+WHERE RowNum > 1;
+GO
+
+
+BEGIN TRY
+    BEGIN TRANSACTION;
+
+    WITH DuplicateCTE AS
+    (
+        SELECT
+            CustomerID,
+            ROW_NUMBER() OVER (
+                PARTITION BY FullName, Email, City
+                ORDER BY CreatedAt ASC
+            ) AS RowNum
+        FROM dbo.Customers
+    )
+    DELETE FROM DuplicateCTE
+    WHERE RowNum > 1;
+
+    COMMIT TRANSACTION;
+    PRINT 'Duplicate records deleted successfully.';
+
+END TRY
+BEGIN CATCH
+    ROLLBACK TRANSACTION;
+
+    PRINT 'Error occurred:';
+    PRINT ERROR_MESSAGE();
+END CATCH;
+GO
